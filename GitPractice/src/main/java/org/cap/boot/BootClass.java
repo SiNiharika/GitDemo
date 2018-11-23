35e1c614af6b0a885a1db967de08b2a978f7c3fe
sinihari <sinihari@DIN73003004.corp.capgemini.com>
2018-11-23 14:38:36 +0530

    First Commit

---
+++ GitPractice/src/main/java/org/cap/boot/BootClass.java
@@ -0,0 +1,15 @@
+package org.cap.boot;
+
+public class BootClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Main Class....");
+		method1();
+	}
+	
+	public static void method1() {
+		System.out.println("Method implementation....");
+	}
+
+}
