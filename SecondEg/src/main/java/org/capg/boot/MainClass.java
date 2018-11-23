d5e98193a046cf100a8203a1044d0b430809a4d9
shreykpa <shreykpa@DIN73002976.corp.capgemini.com>
2018-11-23 15:42:51 +0530

    Shrey Commit

--- SecondEg/src/main/java/org/capg/boot/MainClass.java
+++ SecondEg/src/main/java/org/capg/boot/MainClass.java
@@ -9,6 +9,12 @@ public class MainClass {
 	
 	public static void method2() {
 		System.out.println("Method2 implementation....");
+		System.out.println("Shrey is Cool....");
+		System.out.println("Niha is Puchki....");
+		System.out.println("Ishu is sasti....");
 	}
+	
+	
+	
 
 }
